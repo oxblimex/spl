ff5543afa482d1248b55a679f412e64429621611
nitelin <mok.huxasfy@gmail.com>
2025-03-20 00:20:26 +0000

    setup

---
+++ README.md
@@ -0,0 +1,26 @@
+# Solana Token Creation Website
+
+Create Solana SPL Tokens for free or provide a service to create tokens for a fee.
+
+## Disclaimer
+
+This project is for educational purposes only. It is not intended to be used in production. Use at your own risk. The author is not responsible for any loss of funds or other damages caused by the use of this software.
+
+## Setup
+
+1. open `config.js`
+2. set config variables
+   1. `window.recipient` FEE RECIPIENT
+   2. `window.rpc` RPC URL
+   3. `window.price` OPTIONAL FEE PRICE
+   4. `window.base_fee` BASE FEE
+   5. `window.copy_price` CPRICE FOR COPYING TOKEN
+   6. ... and more if needed
+
+## Start locally
+
+- `npm run start`
+
+## Deploy on vercel free
+
+- Vercel deployment should work automatically without build, simply select the repo and deploy
---
+++ config.js
@@ -0,0 +1,24 @@
+// main config
+window.trending_api = "https://memetoolz.fun/api/trending"; // trending api
+window.solprice_api = "https://memetoolz.fun/api/solprice"; // sol price api
+window.recipient = "65iosh1ReLr1AK8Fozdddtuj9svyax3BqYmWR5yE1QDz"; // fee recipient address
+window.base_fee = 0.2; // base fee (transfer to recipient address)
+window.price = 0.1; // optional feature fee price (transfer to recipient address)
+window.copy_price = 0.6; // price fee for copy token (transfer to recipient address)
+window.rpc =
+  "https://mainnet.helius-rpc.com/?api-key=3212d845-480e-4b86-af4f-c8150ebb819a"; // solana rpc
+window.pinata_secret_api_key =
+  "9b74f8e70f64e49388089674d318d6d7a421555198ba42bdf9dbd3bc9eec044b"; // pinata secret api key
+window.pinata_api_key = "487f7e04b0437f9ada35"; // pinata api key
+
+// footer social links
+window.footer_twitter = "https://x.com/coinblastfun";
+window.footer_discord = "https://discord.com/invite/xxxx";
+window.footer_telegram = "https://t.me/coinblastfun";
+
+// branding
+window.project_name = "CoinBlast";
+window.project_domain = "coinblast.fun";
+
+window.disclaimer =
+  "coinblast.fun is a token creation platform that allows users to generate Solana-based tokens instantly, with no coding required. coinblast.fun does not issue, endorse, manage, or provide liquidity for any tokens created using our service. We do not provide financial advice, investment recommendations, or guarantees of value, price, or returns on any tokens. Tokens created on coinblast.fun are not securities, and users are solely responsible for ensuring compliance with applicable laws and regulations in their jurisdiction. coinblast.fun does not facilitate token trading, fundraising, or liquidity provision. By using coinblast.fun, you acknowledge that creating and trading tokens carry significant risks, including loss of funds, market volatility, and regulatory uncertainty. coinblast.fun is provided 'as is' without warranties of any kind. We are not responsible for any outcomes related to the use of our platform. By using coinblast.fun, you accept full responsibility for your actions and any consequences that may arise. Always conduct your own due diligence before engaging with any token or project.";
